9b3fba755bd3125b8fd71f8c65532cc512b32077
Ty Cooper <35544142+tycooperaow@users.noreply.github.com>
2025-07-09 00:22:40 -0400

    1.2.3

---
+++ packages/types/src/types/organization_statement_profile.ts
@@ -0,0 +1,9 @@
+export interface OrganizationStatementProfile {
+    id: string;
+    created_at: number;
+    updated_at: number;
+    status: string;
+    metadata: any;
+    organization_id: string;
+    comission_rate: number;
+}
